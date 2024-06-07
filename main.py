e05680ecce2259ab1851e60267e37cec883f532f
Roy Mathew <rmathew8@gmail.com>
2024-06-07 15:40:21 -0400

    added mock auth0 code.

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
 #!/usr/bin/python
 # coding: utf-8
-from flask import Flask
+
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/hello")
 def entry_point():
     return "Hello World!"
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return jsonify({"msg": "Server is alive"}), 200
+
+
+@app.route("/oauth/token", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    # Validate the request parameters
+    if (
+        data.get("grant_type") != "password"
+        or data.get("client_id") != "your_client_id"
+        or data.get("client_secret") != "your_client_secret"
+    ):
+        return jsonify({"error": "Invalid request parameters"}), 400
+
+    # Validate the username and password
+    if data.get("username") != "test_user" or data.get("password") != "test_password":
+        return jsonify({"error": "Invalid credentials"}), 401
+
+    # Return a mock access token
+    return (
+        jsonify(
+            {
+                "access_token": "mock_access_token",
+                "token_type": "Bearer",
+                "expires_in": 86400,
+            }
+        ),
+        200,
+    )
 
 
 if __name__ == "__main__":
